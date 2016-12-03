b972208b717580bd017c9b93a80e1374ab1b7237
lundgren84 <lundgren84@hotmail.se>
2016-12-03 08:27:19 +0100

    asd

--- MVC_PictureGallery_Lab/MVC_PictureGallery_Lab/JavaScripts/CreateAlbum.js
+++ MVC_PictureGallery_Lab/MVC_PictureGallery_Lab/JavaScripts/CreateAlbum.js
@@ -1,46 +1,46 @@
-﻿//(function () {
-//    //Getting the form
-//    var form = $('#AlbumForm');
+﻿$('ShowAlbumForm').click(function () {
+    //The Show form button
+    var btn = $('ShowAlbumForm')
+    //The formDiv
+    var from = $('AlbumFormDiv')
+    //txtBox AlbumName
+    var txtValue = document.getElementById("txtbox").value;
 
+
+
+});
+
+$('AlbumFormSubmit').click(function () {
+    //The formDiv
+    var from = $('AlbumFormDiv')
+    //txtBox AlbumName
+    var txtValue = document.getElementById("txtbox").value;
+
+
+});
+//(function () {
+//    //Getting the form
+//    var form = document.getElementById("AlbumForm");
 
 //    //Adding a eventListener
 //    form.addEventListener("submit", function (e) {
-//        e.preventDefault();
+       
+//        var text = "";
 
-//                var albumName = $('#txtbox');
+//        var txtValue = document.getElementById("txtbox").value;
 
-//                if(albumName ==""){           
-//                    $.growl.error({ message: "You need a name!" });       
-//                }
-//                else{
-//                    $.growl.notice({ message: "Album created!" });
-//                }
+//        if (txtValue == 0) {
+//            e.preventDefault();
+//            $.growl.error({ message: "You need a name!" });
+//            text = "You need a name!";
+//        }
+//        else {
+//            $.growl.notice({ message: "Album created!" });
+//            text = "Album created!";
+//        }
+//        document.getElementById("JSValidation").innerHTML = text;
 //    })
 //})();
-
-(function () {
-    //Getting the form
-    var form = document.getElementById("AlbumForm");
-
-    //Adding a eventListener
-    form.addEventListener("submit", function (e) {
-       
-        var text = "";
-
-        var txtValue = document.getElementById("txtbox").value;
-
-        if (txtValue == 0) {
-            e.preventDefault();
-            $.growl.error({ message: "You need a name!" });
-            text = "You need a name!";
-        }
-        else {
-            $.growl.notice({ message: "Album created!" });
-            text = "Album created!";
-        }
-        document.getElementById("JSValidation").innerHTML = text;
-    })
-})();
 
 
 
